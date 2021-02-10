f87d155518709bc8394e669680163464a279c964
William Henderson <william-henderson@outlook.com>
2021-02-10 18:24:14 +0000

    Added one byte of flags for button states Currently this is just left click, right click and middle click

--- src/client.rs
+++ src/client.rs
@@ -1,11 +1,13 @@
 use multiinput::*;
-use std::net::UdpSocket;
+use std::{convert::TryInto, net::UdpSocket};
 
 pub struct Client {
     input_manager: RawInputManager,
     socket: UdpSocket,
 }
 
+/// Initialise the client by connecting the UDP socket to the server.
+/// This also registers `multiinput` to listen for mouse events.
 pub fn init(ip: String) -> Client {
     let mut manager = RawInputManager::new().unwrap();
     manager.register_devices(DeviceType::Mice);
@@ -19,18 +21,58 @@ pub fn init(ip: String) -> Client {
     }
 }
 
+/// Run the client, detecting mouse events and sending them to the server
 pub fn run(client: &mut Client) {
+    let mut button_flags: i8 = 0;
+    let mut button_flags_previous: i8;
+
     loop {
-        if let Some(event) = client.input_manager.get_event() {
-            match event {
-                RawEvent::MouseMoveEvent(_, x, y) => transmit(client, x, y),
-                _ => continue,
+        let events = client.input_manager.get_events().collect::<Vec<RawEvent>>();
+
+        // Store the previous button flags to know if they changed
+        button_flags_previous = button_flags;
+
+        // Update the button flags to match the currently held buttons
+        events.iter().for_each(|e| match e {
+            RawEvent::MouseButtonEvent(_, button, state) => {
+                let add_or_subtract = match state {
+                    State::Pressed => 1_i8,
+                    State::Released => -1_i8,
+                };
+
+                button_flags = match button {
+                    MouseButton::Left => button_flags + add_or_subtract,
+                    MouseButton::Right => button_flags + 2 * add_or_subtract,
+                    MouseButton::Middle => button_flags + 4 * add_or_subtract,
+                    _ => button_flags,
+                }
             }
+            _ => (),
+        });
+
+        // If the mouse has moved, send the data and set transmitted to true. Defaults to false
+        let transmitted = events
+            .iter()
+            .find_map(|e| match e {
+                RawEvent::MouseMoveEvent(_, x, y) => Some(transmit(client, x, y, button_flags)),
+                _ => None,
+            })
+            .unwrap_or(false);
+
+        // If not already sent the data this iteration and the button states have changed, send it anyway
+        if !transmitted && button_flags != button_flags_previous {
+            transmit(client, &0, &0, button_flags);
         }
     }
 }
 
-fn transmit(client: &mut Client, x: i32, y: i32) {
-    let to_send = [x.to_le_bytes(), y.to_le_bytes()].concat();
+/// Transmits the mouse's relative movement as well as the button states
+fn transmit(client: &mut Client, x: &i32, y: &i32, button_flags: i8) -> bool {
+    let mut to_send = [x.to_le_bytes(), y.to_le_bytes()].concat();
+    to_send.push(button_flags.try_into().unwrap());
+
     client.socket.send(&to_send).unwrap();
+    println!("transmitting {:?},{:?},{:?}", x, y, button_flags);
+
+    return true;
 }
--- src/server.rs
+++ src/server.rs
@@ -6,9 +6,9 @@ pub struct Server {
     socket: UdpSocket,
 }
 
+/// Initialise the server, starting the UDP socket and returning the server instance
 pub fn init() -> Server {
     let manager = Enigo::new();
-
     let socket = UdpSocket::bind("0.0.0.0:42069").unwrap();
 
     Server {
@@ -17,14 +17,50 @@ pub fn init() -> Server {
     }
 }
 
+/// Run the server, listening for mouse events from all connected clients and moving the mouse accordingly
 pub fn run(server: &mut Server) {
+    let mut button_flags: u8 = 0;
+    let mut previous_button_flags: u8;
+    let button_types: Vec<MouseButton> =
+        vec![MouseButton::Left, MouseButton::Right, MouseButton::Middle];
+
     loop {
-        let mut buf = [0; 8];
+        let mut buf = [0; 9];
         server.socket.recv_from(&mut buf).unwrap();
 
         let x = i32::from_le_bytes(buf[0..4].try_into().unwrap());
         let y = i32::from_le_bytes(buf[4..8].try_into().unwrap());
+        previous_button_flags = button_flags;
+        button_flags = buf[8];
+
+        let button_states = button_flags.to_bools();
+        let previous_button_states = previous_button_flags.to_bools();
+        for button_id in 0..button_types.len() {
+            if button_states[button_id] ^ previous_button_states[button_id] {
+                match button_states[button_id] {
+                    true => server.output_manager.mouse_down(button_types[button_id]),
+                    false => server.output_manager.mouse_up(button_types[button_id]),
+                }
+            }
+        }
 
         server.output_manager.mouse_move_relative(x, y);
+    }
+}
+
+trait Flags {
+    fn to_bools(&self) -> Vec<bool>;
+}
+
+impl Flags for u8 {
+    /// Convert the `u8` into a `Vec<bool>` for each bit.
+    /// The least significant bit becomes the start of the array.
+    fn to_bools(&self) -> Vec<bool> {
+        let mut out: Vec<bool> = Vec::new();
+        for bit in 0..8 {
+            out.push(self & (1 << bit) != 0);
+        }
+
+        return out;
     }
 }
